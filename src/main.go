15b5d5bcf4d6933052dee830e7a574ecc403f11a
Edwin Pers <pers.edwin@gmail.com>
2025-10-20 21:58:51 -0400

    we only want to register once at startup..

--- src/main.go
+++ src/main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"os"
-	"syscall"
 
 	"fmt"
-	"time"
 
-	"github.com/fsnotify/fsnotify"
 	log "github.com/sirupsen/logrus"
-	pluginapi "k8s.io/kubelet/pkg/apis/deviceplugin/v1beta1"
 )
 
 func main() {
@@ -24,17 +20,6 @@ func main() {
 
 	fmt.Printf("config: %#v\n", *hostDevPluginConfig)
 
-	log.Println("Starting FS watcher.")
-	watcher, err := newFSWatcher(pluginapi.DevicePluginPath)
-	if err != nil {
-		log.Println("Failed to created FS watcher.")
-		os.Exit(-1)
-	}
-	defer watcher.Close()
-
-	log.Println("Starting OS watcher.")
-	sigs := newOSWatcher(syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-
 	pluginManager, err := NewHostDevicePluginManager(hostDevPluginConfig)
 	if err != nil {
 		log.Fatal(err)
@@ -44,34 +29,5 @@ func main() {
 
 	if err := pluginManager.Start(); err != nil {
 		log.Fatal(err)
-	}
-
-	ticker := time.NewTicker(time.Second * 5)
-
-L:
-	for {
-		select {
-		case <-ticker.C:
-			pluginManager.RegisterPluginToKubelet()
-
-		case event := <-watcher.Events:
-			if event.Name == pluginapi.KubeletSocket && event.Op&fsnotify.Create == fsnotify.Create {
-				log.Printf("inotify: %s created, restarting.", pluginapi.KubeletSocket)
-				pluginManager.RegisterPluginToKubelet()
-			}
-
-		case err := <-watcher.Errors:
-			log.Printf("inotify: %s", err)
-
-		case s := <-sigs:
-			switch s {
-			case syscall.SIGHUP:
-				log.Println("Received SIGHUP, restarting.")
-			default:
-				log.Printf("Received signal \"%v\", shutting down.", s)
-				pluginManager.Stop()
-				break L
-			}
-		}
 	}
 }
